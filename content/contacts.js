45eabbdf8905eecd8a4f5df30deb8149a2d41e24
sujiali <sujiali.reg@163.com>
2015-11-17 20:44:14 +0800

    myContacts initial

---
+++ content/contacts.js
@@ -0,0 +1,28 @@
+Components.utils.import("resource://gre/modules/Services.jsm");
+Components.utils.import("resource://gre/modules/FileUtils.jsm");
+
+var file = FileUtils.getFile("ProfD", ["test1.sqlite"]);
+var dbConn = Services.storage.openDatabase(file); // Will also create the file
+// if it does not exist
+var statement = dbConn.createStatement("SELECT * FROM text");
+
+statement.executeAsync({
+	handleResult : function(aResultSet) {
+		for (let row = aResultSet.getNextRow(); row; row = aResultSet
+				.getNextRow()) {
+			var value = row.getResultByName("Tel");
+			console.log(value);
+		}
+	},
+
+	handleError : function(aError) {
+		print("Error: " + aError.message);
+	},
+
+	handleCompletion : function(aReason) {
+		if (aReason != Components.interfaces.mozIStorageStatementCallback.REASON_FINISHED)
+			print("Query canceled or aborted!");
+	}
+});
+
+// console.log(statement.getColumnName());
