33524cfca2abe770c08ba0b3229bb87074457bdd
sujiali <sujiali.reg@163.com>
2015-11-25 15:15:20 +0800

    connect tree with sqlite database

--- content/contacts.js
+++ content/contacts.js
@@ -1,29 +1,29 @@
-Components.utils.import("resource://gre/modules/Services.jsm");
-Components.utils.import("resource://gre/modules/FileUtils.jsm");
-
-var file = FileUtils.getFile("ProfD", ["test1.sqlite"]);
-var dbConn = Services.storage.openDatabase(file); // Will also create the file
-// if it does not exist
-var statement = dbConn.createStatement("SELECT * FROM text");
-statement.executeAsync({
-	handleResult : function(aResultSet) {
-		// add comment
-		console.log(aResultSet);
-		for (let row = aResultSet.getNextRow(); row; row = aResultSet
-				.getNextRow()) {
-			var value = row.getResultByName("Tel");
-			console.log(value);
-		}
-	},
-
-	handleError : function(aError) {
-		print("Error: " + aError.message);
-	},
-
-	handleCompletion : function(aReason) {
-		if (aReason != Components.interfaces.mozIStorageStatementCallback.REASON_FINISHED)
-			print("Query canceled or aborted!");
-	}
-});
-
-// console.log(statement.getColumnName());
+//Components.utils.import("resource://gre/modules/Services.jsm");
+//Components.utils.import("resource://gre/modules/FileUtils.jsm");
+//
+//var file = FileUtils.getFile("ProfD", ["test1.sqlite"]);
+//var dbConn = Services.storage.openDatabase(file); // Will also create the file
+//// if it does not exist
+//var statement = dbConn.createStatement("SELECT * FROM text");
+//statement.executeAsync({
+//	handleResult : function(aResultSet) {
+//		// add comment
+//		console.log(aResultSet);
+//		for (let row = aResultSet.getNextRow(); row; row = aResultSet
+//				.getNextRow()) {
+//			var value = row.getResultByName("Tel");
+//			console.log(value);
+//		}
+//	},
+//
+//	handleError : function(aError) {
+//		print("Error: " + aError.message);
+//	},
+//
+//	handleCompletion : function(aReason) {
+//		if (aReason != Components.interfaces.mozIStorageStatementCallback.REASON_FINISHED)
+//			print("Query canceled or aborted!");
+//	}
+//});
+//
+//// console.log(statement.getColumnName());
