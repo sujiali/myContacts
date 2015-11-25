4a7fe528084740420005484bf05e4a8c58454f7a
sujiali <sujiali.reg@163.com>
2015-11-25 23:22:34 +0800

    tree autosize

--- content/contacts.js
+++ content/contacts.js
@@ -1,29 +1,27 @@
-//Components.utils.import("resource://gre/modules/Services.jsm");
-//Components.utils.import("resource://gre/modules/FileUtils.jsm");
-//
-//var file = FileUtils.getFile("ProfD", ["test1.sqlite"]);
-//var dbConn = Services.storage.openDatabase(file); // Will also create the file
-//// if it does not exist
-//var statement = dbConn.createStatement("SELECT * FROM text");
-//statement.executeAsync({
-//	handleResult : function(aResultSet) {
-//		// add comment
-//		console.log(aResultSet);
-//		for (let row = aResultSet.getNextRow(); row; row = aResultSet
-//				.getNextRow()) {
-//			var value = row.getResultByName("Tel");
-//			console.log(value);
-//		}
-//	},
-//
-//	handleError : function(aError) {
-//		print("Error: " + aError.message);
-//	},
-//
-//	handleCompletion : function(aReason) {
-//		if (aReason != Components.interfaces.mozIStorageStatementCallback.REASON_FINISHED)
-//			print("Query canceled or aborted!");
-//	}
-//});
-//
-//// console.log(statement.getColumnName());
+Components.utils.import("resource://gre/modules/Services.jsm");
+Components.utils.import("resource://gre/modules/FileUtils.jsm");
+
+var file = FileUtils.getFile("ProfD", ["test1.sqlite"]);
+var fieldname = new Array();
+var dbConn = Services.storage.openDatabase(file); // Will also create the file
+// if it does not exist
+var statement = dbConn.createStatement("PRAGMA table_info([text])");
+
+while (statement.executeStep()) {
+	fieldname.push(statement.row.name);
+}
+
+// var fieldname = ["old_id", "FullName", "Tel"];
+// console.log(fieldname);
+// console.log(fieldname.length);
+
+for (let i = 0; i < fieldname.length; i++) {
+	let tcol = document.createElement("treecol");
+	tcol.setAttribute("label", fieldname[i]);
+	tcol.setAttribute("flex", i);
+	document.getElementById("mycols").appendChild(tcol);
+	//console.log(fieldname[i]);
+	let tcell = document.createElement("treecell");
+	tcell.setAttribute("label", "?" + fieldname[i]);
+	document.getElementById("mycells").appendChild(tcell);
+}
