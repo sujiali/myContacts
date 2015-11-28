d6bf6375bb74394b78bdeaf2eebf302e9c73f536
sujiali <sujiali.reg@163.com>
2015-11-29 01:19:36 +0800

    switch in readOnly and editable. the delete fucntion is to do.

--- content/newwin.js
+++ content/newwin.js
@@ -1,42 +1,22 @@
-//Components.utils.import("resource://gre/modules/Services.jsm");
-//Components.utils.import("resource://gre/modules/FileUtils.jsm");
-
-function dosql() {
-	//var file = FileUtils.getFile("ProfD", ["test1.sqlite"]);
-	//var fieldname = new Array();
-	//var dbConn = Services.storage.openDatabase(file); // Will also create the file if it does not exist
-	//
-	//// get field name
-	//var statement = dbConn.createStatement("PRAGMA table_info([text])");
-	//while (statement.executeStep()) {
-	//	fieldname.push(statement.row.name);
-	//}
-
-	// var fieldname = ["old_id", "FullName", "Tel"];
-	// console.log(fieldname);
-	// console.log(fieldname.length);
+function openlist() {
 	var fieldname = new Array();
 	var mylist = document.getElementById("mylist");
 	for (let i = 1; i < mylist.childNodes.length; i++) {
 		if (mylist.childNodes[i].getAttribute("checked") == "true") {
 			fieldname.push(mylist.childNodes[i].getAttribute("label"));
 		}
-
 	}
-
-
 	// update tree
 	var mycols = document.getElementById("mycols");
 	var mycells = document.getElementById("mycells");
 	removeAllChild(mycols);
 	removeAllChild(mycells);
-
 	for (let i = 0; i < fieldname.length; i++) {
 		let tcol = document.createElement("treecol");
 		tcol.setAttribute("label", fieldname[i]);
 		tcol.setAttribute("flex", i);
 		mycols.appendChild(tcol);
-		
+
 		let tcell = document.createElement("treecell");
 		tcell.setAttribute("label", "?" + fieldname[i]);
 		mycells.appendChild(tcell);
@@ -52,6 +32,11 @@ function removeAllChild(obj) {
 
 var mytree = document.getElementById("mytree");
 mytree.addEventListener("dblclick", function(event) {
-    let selectrecord=(mytree.view.getCellText(mytree.currentIndex, mytree.columns.getColumnAt(0)));
-    window.openDialog('chrome://myContacts/content/update.xul','showmore','chrome,width=600,height=300',selectrecord);
+	var selectrecord = (mytree.view.getCellText(mytree.currentIndex, mytree.columns.getColumnAt(0)));
+	window.openDialog('chrome://myContacts/content/update.xul', 'showmore', 'chrome,width=600,height=300', selectrecord);
 }, true);
+
+function addnew() {
+	var selectrecord="";
+	window.openDialog('chrome://myContacts/content/update.xul', 'showmore', 'chrome,width=600,height=300', selectrecord);
+}
--- content/update.js
+++ content/update.js
@@ -3,14 +3,42 @@
 
 Components.utils.import("resource://gre/modules/Services.jsm");
 Components.utils.import("resource://gre/modules/FileUtils.jsm");
+var recordId = window.arguments[0];
 var record = {};
-getbyid(window.arguments[0], function(rows) {
-    record = rows;
-});
+var textboxs = new Array("fullname", "tel");
+var buttonSwitch = {
+    on: "edit",
+    off: "lock"
+};
+var textboxSwitch = {
+    on: "edit_style",
+    off: "read_style"
+};
+if (recordId == "") {
+    // new one;
+    document.getElementById("tid").value = "This is new one, the record id is undefine.";
+    for (let i = 0; i < textboxs.length; i++) {
+        document.getElementById(textboxs[i]).setAttribute("class", textboxSwitch.on);
+    }
+    document.getElementById("switchRead").setAttribute("label", buttonSwitch.off);
+} else {
+    // already exsit
+    getbyid(window.arguments[0], function(rows) {
+        record = rows;
+        document.getElementById("tid").value = "The record id is " + record.old_id;
+        document.getElementById("fullname").value = record.FullName;
+        document.getElementById("tel").value = record.Tel;
+        for (let i = 0; i < textboxs.length; i++) {
+            document.getElementById(textboxs[i]).readOnly = true;
+            document.getElementById(textboxs[i]).setAttribute("class", textboxSwitch.off);
+        }
+        document.getElementById("switchRead").setAttribute("label", buttonSwitch.on);
+    });
+}
 
 function getbyid(id, _callback) {
     var file = FileUtils.getFile("ProfD", ["test1.sqlite"]);
-    var trecord = {};
+    var record = {};
     var dbConn = Services.storage.openDatabase(file); // Will also create the file if it does not exist
     // var statement = dbConn.createStatement("update text set FullName='susu', Tel='123' where old_id='70'");
     var statement = dbConn.createStatement("select * from text where old_id= :id");
@@ -18,9 +46,9 @@ function getbyid(id, _callback) {
     statement.executeAsync({
         handleResult: function(aResultSet) {
             for (let row = aResultSet.getNextRow(); row; row = aResultSet.getNextRow()) {
-                trecord.old_id = row.getResultByName("old_id");
-                trecord.FullName = row.getResultByName("FullName");
-                trecord.Tel = row.getResultByName("Tel");
+                record.old_id = row.getResultByName("old_id");
+                record.FullName = row.getResultByName("FullName");
+                record.Tel = row.getResultByName("Tel");
             }
         },
         handleError: function(aError) {
@@ -30,25 +58,37 @@ function getbyid(id, _callback) {
             if (aReason != Components.interfaces.mozIStorageStatementCallback.REASON_FINISHED) {
                 console.log("Query canceled or aborted!");
             }
-            document.getElementById("tid").setAttribute("value", "The record id is " + trecord.old_id);
-            document.getElementById("fullname").setAttribute("value", trecord.FullName);
-            document.getElementById("tel").setAttribute("value", trecord.Tel);
-            _callback(trecord);
+            _callback(record);
         }
     });
     statement.finalize();
 }
 
+function statusChange() {
+    for (let i = 0; i < textboxs.length; i++) {
+        document.getElementById(textboxs[i]).readOnly = !(document.getElementById(textboxs[i]).readOnly);
+        switchAttr(document.getElementById(textboxs[i]), "class", textboxSwitch);
+    }
+    switchAttr(document.getElementById("switchRead"), "label", buttonSwitch);
+}
+
 function doOK() {
+    record.FullName = document.getElementById("fullname").value;
+    record.Tel = document.getElementById("tel").value;
+    
     var file = FileUtils.getFile("ProfD", ["test1.sqlite"]);
     var fieldname = new Array();
     var dbConn = Services.storage.openDatabase(file); // Will also create the file if it does not exist
-    record.FullName = document.getElementById("fullname").value;
-    record.Tel = document.getElementById("tel").value;
-    var statement = dbConn.createStatement("update text set FullName = :fn, Tel = :tel where old_id = :id");
+    if (recordId == "") {
+        // new one
+        var statement = dbConn.createStatement("insert into text (FullName, Tel) values (:fn, :tel)");
+    } else {
+        // already exsit
+        var statement = dbConn.createStatement("update text set FullName = :fn, Tel = :tel where old_id = :id");
+        statement.params.id = record.old_id;
+    }
     statement.params.fn = record.FullName;
     statement.params.tel = record.Tel;
-    statement.params.id = record.old_id;
     statement.execute();
     statement.finalize();
     return true;
@@ -57,4 +97,12 @@ function doOK() {
 function doCancel() {
     console.log("You pressed Cancel!");
     return true;
+}
+
+function switchAttr(obj, attr, status_obj) {
+    if (obj.getAttribute(attr) == status_obj.on) {
+        obj.setAttribute(attr, status_obj.off);
+    } else if (obj.getAttribute(attr) == status_obj.off) {
+        obj.setAttribute(attr, status_obj.on);
+    }
 }
