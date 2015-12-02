7d083b75e232234c2ac15f3674f941526cde675c
sujiali <sujiali.reg@163.com>
2015-12-02 22:26:14 +0800

    interact with tags

--- content/newwin.js
+++ content/newwin.js
@@ -1,42 +1,44 @@
-function openlist() {
-	var fieldname = new Array();
-	var mylist = document.getElementById("mylist");
-	for (let i = 1; i < mylist.childNodes.length; i++) {
-		if (mylist.childNodes[i].getAttribute("checked") == "true") {
-			fieldname.push(mylist.childNodes[i].getAttribute("label"));
-		}
-	}
-	// update tree
-	var mycols = document.getElementById("mycols");
-	var mycells = document.getElementById("mycells");
-	removeAllChild(mycols);
-	removeAllChild(mycells);
-	for (let i = 0; i < fieldname.length; i++) {
-		let tcol = document.createElement("treecol");
-		tcol.setAttribute("label", fieldname[i]);
-		tcol.setAttribute("flex", i);
-		mycols.appendChild(tcol);
-
-		let tcell = document.createElement("treecell");
-		tcell.setAttribute("label", "?" + fieldname[i]);
-		mycells.appendChild(tcell);
-	}
-	document.getElementById("mytree").builder.rebuild();
+function addnew() {
+	var selectrecord = "";
+	window.openDialog('chrome://myContacts/content/update.xul', 'showmore', 'chrome,width=600,height=300', selectrecord);
 }
-
-function removeAllChild(obj) {
-	while (obj.hasChildNodes()) {
-		obj.removeChild(obj.firstChild);
-	}
-}
-
 var mytree = document.getElementById("mytree");
 mytree.addEventListener("dblclick", function(event) {
 	var selectrecord = (mytree.view.getCellText(mytree.currentIndex, mytree.columns.getColumnAt(0)));
 	window.openDialog('chrome://myContacts/content/update.xul', 'showmore', 'chrome,width=600,height=300', selectrecord);
 }, true);
 
-function addnew() {
-	var selectrecord="";
-	window.openDialog('chrome://myContacts/content/update.xul', 'showmore', 'chrome,width=600,height=300', selectrecord);
-}
+var mylist = document.getElementById("mylist");
+mylist.addEventListener("click", function(event) {
+	// define
+	let selectedTag = mylist.selectedItems[0].getAttribute("label");
+	let mytemplate = document.getElementById("mytemplate");
+	let myquery1 = document.getElementById("myquery");
+	let myaction = document.getElementById("myaction");
+	
+	// clear
+	mytemplate.removeChild(myquery1);
+
+	// insert new query
+	if (selectedTag == "All") {
+		let myquery2 = document.createElement("query");
+		myquery2.setAttribute("id", "myquery");
+		let statement = 'SELECT * FROM text'
+		myquery2.textContent = statement;
+		mytemplate.insertBefore(myquery2, myaction);
+	} else {
+		let myquery2 = document.createElement("query");
+		myquery2.setAttribute("id", "myquery");
+		let statement = 'SELECT * FROM text where old_id in (select textid from relation where tagid in (select id from tags where tagname = :ss))'
+		myquery2.textContent = statement;
+		mytemplate.insertBefore(myquery2, myaction);
+
+		var myquery = document.getElementById("myquery");
+		let myparam = document.createElement("param");
+		myparam.setAttribute("name", "ss");
+		myparam.textContent = selectedTag;
+		myquery.appendChild(myparam);
+	}
+	document.getElementById("mytree").builder.rebuild();
+	// var statement2 = 'SELECT * FROM text where old_id in (select textid from relation where tagid in (select id from tags where tagname = :s))'
+}, true);
--- content/update.js
+++ content/update.js
@@ -1,9 +1,10 @@
 /* Copyright (c) 2006 YourNameHere
    See the file LICENSE.txt for licensing information. */
 
+// define
 Components.utils.import("resource://gre/modules/Services.jsm");
 Components.utils.import("resource://gre/modules/FileUtils.jsm");
-var recordId = window.arguments[0];
+var tId = window.arguments[0];
 var record = {};
 var textboxs = new Array("fullname", "tel");
 var buttonSwitch = {
@@ -14,17 +15,18 @@ var textboxSwitch = {
     on: "edit_style",
     off: "read_style"
 };
-if (recordId == "") {
-    // new one;
+
+if (tId == "") {
+    // insert record;
     document.getElementById("tid").value = "This is new one, the record id is undefine.";
     for (let i = 0; i < textboxs.length; i++) {
         document.getElementById(textboxs[i]).setAttribute("class", textboxSwitch.on);
     }
     document.getElementById("switchRead").setAttribute("label", buttonSwitch.off);
 } else {
-    // already exsit
-    getbyid(window.arguments[0], function(rows) {
-        record = rows;
+    // update record;
+    selectRecordById(tId, function(aRecord) {
+        record = aRecord;
         document.getElementById("tid").value = "The record id is " + record.old_id;
         document.getElementById("fullname").value = record.FullName;
         document.getElementById("tel").value = record.Tel;
@@ -36,19 +38,18 @@ if (recordId == "") {
     });
 }
 
-function getbyid(id, _callback) {
+function selectRecordById(id, _callback) {
+    var aRecord = {};
     var file = FileUtils.getFile("ProfD", ["test1.sqlite"]);
-    var record = {};
     var dbConn = Services.storage.openDatabase(file); // Will also create the file if it does not exist
-    // var statement = dbConn.createStatement("update text set FullName='susu', Tel='123' where old_id='70'");
     var statement = dbConn.createStatement("select * from text where old_id= :id");
     statement.params.id = id;
     statement.executeAsync({
         handleResult: function(aResultSet) {
             for (let row = aResultSet.getNextRow(); row; row = aResultSet.getNextRow()) {
-                record.old_id = row.getResultByName("old_id");
-                record.FullName = row.getResultByName("FullName");
-                record.Tel = row.getResultByName("Tel");
+                aRecord.old_id = row.getResultByName("old_id");
+                aRecord.FullName = row.getResultByName("FullName");
+                aRecord.Tel = row.getResultByName("Tel");
             }
         },
         handleError: function(aError) {
@@ -58,13 +59,13 @@ function getbyid(id, _callback) {
             if (aReason != Components.interfaces.mozIStorageStatementCallback.REASON_FINISHED) {
                 console.log("Query canceled or aborted!");
             }
-            _callback(record);
+            _callback(aRecord);
         }
     });
     statement.finalize();
 }
 
-function statusChange() {
+function switchStatus() {
     for (let i = 0; i < textboxs.length; i++) {
         document.getElementById(textboxs[i]).readOnly = !(document.getElementById(textboxs[i]).readOnly);
         switchAttr(document.getElementById(textboxs[i]), "class", textboxSwitch);
@@ -75,11 +76,10 @@ function statusChange() {
 function doOK() {
     record.FullName = document.getElementById("fullname").value;
     record.Tel = document.getElementById("tel").value;
-    
     var file = FileUtils.getFile("ProfD", ["test1.sqlite"]);
     var fieldname = new Array();
     var dbConn = Services.storage.openDatabase(file); // Will also create the file if it does not exist
-    if (recordId == "") {
+    if (tId == "") {
         // new one
         var statement = dbConn.createStatement("insert into text (FullName, Tel) values (:fn, :tel)");
     } else {
