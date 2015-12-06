0f90df43ebf42e6b9815c284fc99b1d01349b566
sujiali <sujiali.reg@163.com>
2015-12-06 23:11:05 +0800

    interact with alpha tabs

--- content/newwin.js
+++ content/newwin.js
@@ -1,9 +1,48 @@
 Components.utils.import("resource://gre/modules/Services.jsm");
 Components.utils.import("resource://gre/modules/FileUtils.jsm");
+
+
+Array.prototype.contains = function(obj) {
+	if (this.length == 0) {
+		return true;
+	} else {
+		var i = this.length;
+		while (i--) {
+			if (this[i] === obj) {
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
+function unique(arr) {
+	var result = [],
+		hash = {};
+	for (var i = 0, elem;
+	(elem = arr[i]) != null; i++) {
+		if (!hash[elem]) {
+			result.push(elem);
+			hash[elem] = true;
+		}
+	}
+	return result;
+}
+
+function prop2arr(arr, prop) {
+	var r = [];
+	for (i = 0; i < arr.length; i++) {
+		r.push(arr[i][prop]);
+	}
+	return r;
+}
+
 var cts = {
 	data: [],
 
-	table: [],
+	tabletags: [],
+
+	alphafilter: "",
 
 	tagfilter: [],
 
@@ -19,7 +58,8 @@ var cts = {
 			adata.push({
 				old_id: statement.row.old_id,
 				fullname: statement.row.FullName,
-				tel: statement.row.Tel
+				tel: statement.row.Tel,
+				initcap: statement.row.initcap
 			});
 		}
 		statement.finalize();
@@ -32,12 +72,10 @@ var cts = {
 		// let selectedTag = mylist.selectedItems[0].getAttribute("label");
 		var file = FileUtils.getFile("ProfD", ["test1.sqlite"]);
 		var dbConn = Services.storage.openDatabase(file); // Will also create the file if it does not exist
-
 		var statement = dbConn.createStatement("SELECT tagname from tags");
 		while (statement.executeStep()) {
 			tags.push(statement.row.tagname);
 		}
-
 		for (let i = 0; i < tags.length; i++) {
 			statement.reset();
 			var tgrp = new Array();
@@ -57,29 +95,67 @@ var cts = {
 		var mytree = document.getElementById("mytree");
 		this.data = this.loaddata();
 		this.table = this.data;
+		this.settab();
 		mytree.view = new treeView(this.table);
 	},
 
-	refresh: function() {
-		var mytree = document.getElementById("mytree");
-		if (this.tagfilter.length == 0) {
-			this.table = this.data;
-		} else {
-			this.table = [];
-			for (i = 0; i < this.data.length; i++) {
-				if (this.tagfilter.contains(this.data[i].old_id)) {
-					this.table.push(this.data[i]);
-				}
+	settab: function() {
+		var initcaparr = [];
+		initcaparr = prop2arr(this.table, "initcap");
+		var uni_initcap = unique(initcaparr);
+		var allalpha = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+		for (i = 0; i < allalpha.length; i++) {
+			if (uni_initcap.contains(allalpha[i])) {
+				document.getElementById("tab" + allalpha[i]).setAttribute("hidden", "false");
+			} else {
+				document.getElementById("tab" + allalpha[i]).setAttribute("hidden", "true");
 			}
 		}
+		document.getElementById("tablist").selectedIndex = 0;
+	},
+
+	refresh: function(whr) {
+		var mytree = document.getElementById("mytree");
+		switch (whr) {
+			case "tags":
+				this.table = [];
+				for (let i = 0; i < this.data.length; i++) {
+					var istaged = this.tagfilter.contains(this.data[i].old_id);
+					if (istaged) {
+						this.table.push(this.data[i]);
+					}
+				}
+				this.alphafilter = "";
+				this.settab();
+				break;
+			case "alphas":
+				this.table = [];
+				for (let i = 0; i < this.data.length; i++) {
+					var istaged = this.tagfilter.contains(this.data[i].old_id);
+					if (this.alphafilter == "") {
+						var isalphaed = true;
+					} else if (this.data[i].initcap.indexOf(this.alphafilter) >= 0) {
+						var isalphaed = true;
+					} else {
+						var isalphaed = false;
+					}
+					if (istaged && isalphaed) {
+						this.table.push(this.data[i]);
+					}
+				}
+				break;
+			default:
+				break;
+		}
 		mytree.view = new treeView(this.table);
+
 	},
 
 	addnew: function() {
 		var selectrecord = "";
 		window.openDialog('chrome://myContacts/content/update.xul', 'showmore',
 			'chrome,width=600,height=300', selectrecord);
-	}
+	},
 
 };
 
@@ -123,24 +199,15 @@ function treeView(ttable) {
 
 	var mylist = document.getElementById("mylist");
 	mylist.addEventListener("click", function(event) {
+		// set tagfilter
 		let selectedTag = mylist.selectedItems[0].getAttribute("label");
 		if (selectedTag === "All") {
 			cts.tagfilter = [];
 		} else {
 			cts.tagfilter = cts.tagidx[selectedTag];
 		}
-		cts.refresh();
+		// set alphafilter
+
+		cts.refresh('tags');
 	}, true);
-
-	cts.init();
 })();
-
-Array.prototype.contains = function(obj) {
-	var i = this.length;
-	while (i--) {
-		if (this[i] === obj) {
-			return true;
-		}
-	}
-	return false;
-}
